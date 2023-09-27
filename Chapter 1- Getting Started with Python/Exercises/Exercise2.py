f8975ee5e7d7d5d8d745ce0501e40804b4d70d56
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-27 16:07:43 +0800

    Exercise2.py

--- Chapter 1- Getting Started with Python/Exercises/Exercise2.py
+++ Chapter 1- Getting Started with Python/Exercises/Exercise2.py
@@ -1,7 +1,4 @@
-"""
-Exercise 2: Print the Version of Python :ballot_box_with_check:
-    Write a Python program to get the Python version you are using.
-"""
+
 import platform
 print(platform.python_version())
 
