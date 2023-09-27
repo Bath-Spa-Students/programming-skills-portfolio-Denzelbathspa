329545841634dcc1513986aceb6c3c51f0719b13
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-27 15:59:02 +0800

    Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,11 @@
+"""
+Exercise 4: Strings Concatination :ballot_box_with_check:
+    Write three strings in different variables and print the output as one string.
+"""
+string1 = "Hello my name is "
+string2 = "Denzel lorenz M. Menioria. It is "
+string3 = "nice to meet you! "
+
+# Combining the 3 strings in to one string
+sentence = string1+string2+string3
+print(sentence)
