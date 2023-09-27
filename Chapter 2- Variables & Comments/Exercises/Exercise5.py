1162dbac9322aa36f293e55a528381117cd5854a
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-27 16:43:34 +0800

    Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,10 @@
+
+print("USB sticks cost: £6",
+      "\nCurrent Balance: £50")
+usb = 6
+cb = 50
+
+totalUSB = int(cb/usb)
+balanceLeft = cb-(totalUSB*usb)
+print("Total USB that can be bought with current balance: £",totalUSB,
+      "\nTotal balance remaining: £", balanceLeft)
