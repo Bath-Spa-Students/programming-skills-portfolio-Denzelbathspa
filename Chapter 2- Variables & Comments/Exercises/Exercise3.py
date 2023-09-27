89f299d51e484d15ff9629285c175b8cc2fae3c3
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-27 16:43:15 +0800

    Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,8 @@
+
+famousP = "\tNelson Mandela once said, "
+quote = "\n\"The greatest glory in living lies not in never falling, but in rising every time we fall.\""
+
+print("\n",famousP+quote)
+print("\n",famousP.strip()+quote)
+print("\n",famousP.lstrip("Nelson ")+quote)
+print("\n",famousP+quote.rstrip("fall.\""),"\n")
