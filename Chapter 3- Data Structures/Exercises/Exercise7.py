fc7f51f8ccd17d4b144ca6ddb37eb4c42618ded5
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-28 14:33:07 +0800

    Exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,16 @@
+
+pOG = ["Philippines","London","Japan","Sweden","Canada"]
+
+print(pOG)
+pInOrder = print(sorted(pOG))
+print(pOG)
+pReverse = print(sorted(pOG, reverse=True))
+print(pOG)
+pOG.reverse()
+print(pOG)
+pOG.reverse()
+print(pOG)
+pOG.sort()
+print(pOG)
+pOG.sort(reverse=True)
+print(pOG)
