7eb48903f8af4272538549fd88f49665f7f94673
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-28 14:32:39 +0800

    Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+
+names = ["Beverly","Homer","Dane"]
+message = [", please go to dinner with us. Love you always", ", I would like to invite you to dinner. Please join with us.",", you are invited to dinner please come with us!"]
+
+for x,y in zip(names,message):
+    print(x+y)
