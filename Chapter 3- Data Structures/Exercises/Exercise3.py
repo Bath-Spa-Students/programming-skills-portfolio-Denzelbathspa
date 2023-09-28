5a139c0fa851647cd762f614a83e99609efacefc
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-09-28 14:32:31 +0800

    Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,8 @@
+
+transpo = ["Infinity G37xs","Nissan GTR R35","Porsche 911", "Mini Cooper", "Honda Civic type R", "Nissan 370z", "Honda Insight"]
+
+for x in transpo:
+    if (x == "Mini Cooper" or x == "Honda Insight"):
+        print("I do not want to own a "+x)
+    else:
+        print("I would like to own a "+x)
