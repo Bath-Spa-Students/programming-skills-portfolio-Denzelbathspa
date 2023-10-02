2f355d3ae7da7ea87c66c2cf589010fe19fa8921
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-02 16:23:34 +0800

    Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,14 @@
+
+programWords = {"Variables":" : stores data.",
+                "Comments":" : can only be read by a person not by the machine.",
+                "If":" : is a conditional statement that executes only with the right conditions.",
+                "elif": " : is a conditional statement that executes after an if statement. Works the same as an IF statements.",
+                "else":" : executes only after if and elif statements. Only condition it has is if all the IF statements are false."}
+
+kList = list(programWords.keys())
+vList = list(programWords.values())
+print(kList[0]+vList[0])
+print(kList[1]+vList[1])
+print(kList[2]+vList[2])
+print(kList[3]+vList[3])
+print(kList[4]+vList[4])
