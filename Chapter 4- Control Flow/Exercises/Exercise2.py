ed76f4797dd9774c2ade84690820e652286d20dd
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-02 13:43:15 +0800

    Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,13 @@
+
+import random
+alien_color = ["green","yellow","red"]
+playerPoints = 0
+
+rChoice = random.choice(alien_color)
+print("Player shot a "+rChoice+" alien")
+if rChoice == "green":
+    print("Player Just earned 5 points")
+    playerPoints+=5
+else:
+    print("Player Just earned 10 points")
+    playerPoints+=10
