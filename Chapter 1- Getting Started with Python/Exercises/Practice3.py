8d73f273690df5d2dc91cb09173bfab14b77736a
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-04 16:13:22 +0800

    Practice3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice3.py
@@ -0,0 +1,7 @@
+"""
+Q3: You need to make a program for a leaderboard. The program needs to output the numbers 1 to 9, each on a separate
+line followed by a dot.
+"""
+for x in range(9):
+    y=str(x)
+    print(y+".")
