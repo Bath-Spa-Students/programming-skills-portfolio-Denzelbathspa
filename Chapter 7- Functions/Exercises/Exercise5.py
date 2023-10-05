e37ffd12e742f427d52918e901c869e1802c3ccb
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-05 14:55:07 +0800

    Exercise5.py

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,7 @@
+def describe_city(city="Sharjah",country="United Arab Emirates"):
+    print(""+city+" is in the country of "+country)
+
+
+describe_city("Dubai")
+describe_city("Ras Al-Khaimah")
+describe_city("Manila","Philippines")
