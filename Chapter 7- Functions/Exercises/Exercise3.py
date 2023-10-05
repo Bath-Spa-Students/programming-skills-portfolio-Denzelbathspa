dc3ccefd5c4e4a63375305cb8bc4586eca84d242
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-05 14:30:02 +0800

    Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,9 @@
+def make_shirt(size,txtOnShirt):
+    print("The shirt size is "+str(size)+" and the text message print of the shirt should say: "+txtOnShirt)
+
+
+shirt_TXT_1 = "Im Chill Like That"
+shirt_size_1 = "XL"
+
+make_shirt(shirt_size_1, shirt_TXT_1)
+make_shirt(size = "S", txtOnShirt = "Im a Cool GUY")
