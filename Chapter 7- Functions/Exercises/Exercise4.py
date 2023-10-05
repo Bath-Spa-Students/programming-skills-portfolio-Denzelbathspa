370a6c97c1efa4bc04783d4bbe16a558a2a4aead
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-05 14:54:56 +0800

    Exercise4.py

---
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -0,0 +1,15 @@
+def large_shirt(size="large",txtOnShirt="I love Python"):
+    print("The shirt size is "+str(size)+" and the text message print of the shirt should say: "+txtOnShirt)
+def mediumm_shirt(size="medium",txtOnShirt="I love Python"):
+    print("The shirt size is "+str(size)+" and the text message print of the shirt should say: "+txtOnShirt)
+def any_size_shirt(size,txtOnShirt):
+    print("The shirt size is "+str(size)+" and the text message print of the shirt should say: "+txtOnShirt)
+
+
+shirt_TXT_1 = "Im Chill Like That"
+shirt_size_1 = "XL"
+
+large_shirt()
+mediumm_shirt()
+any_size_shirt(size = "S", txtOnShirt = "Im a Cool GUY")
+any_size_shirt(shirt_size_1, shirt_TXT_1)
