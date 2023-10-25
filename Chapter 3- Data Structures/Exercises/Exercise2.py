4af201d171d02ef554aed243577518078424f6aa
Denzelbathspa <144213582+Denzelbathspa@users.noreply.github.com>
2023-10-25 12:27:39 +0400

    Exercise2.py

--- Chapter 3- Data Structures/Exercises/Exercise2.py
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -1,8 +1,3 @@
-"""
-Start with the list you used in Exercise 1, but instead of just
-printing each person’s name, print a message to them. The text of each message should be the same, but each message should be 
-personalized with the person’s name.
-"""
 names = ["Jade","Thontie","Earl","Drazen","Gali","Rovic","Eminem","Ian"]
 message = ", you are one of the most wonderfull friend I could ask for. Thank you for being with me."
 
